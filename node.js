665e2f6a26f599e5ed0ade2d1f38a9fca9d1082e
Falguni Jethva <92168604+falgunijethva@users.noreply.github.com>
2021-10-09 16:15:03 +0000

    run

--- node.js
+++ node.js
@@ -1,13 +1,13 @@
 const os = require("os");
 
-console.log("\n Operating System Architecture :- " + os.arch());
+console.log("\n OS CPU Architecture :- " + os.arch());
 
 console.log("\n Free Memory Of The Operating System :- " + 
 os.freemem()/(1024*1024*1024));
 
 console.log("\n Total Memory Of The Operating system :- " + os.totalmem()/(1024*1024*1024));
 
-console.log("\n Operating System Platform :-" + os.platform());
+console.log("\n OS Platform :-" + os.platform());
 
 console.log("\n Display Information About The Current Looged User :-");
 
